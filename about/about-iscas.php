7145bbe9884a44b2a045ebe7b191fcc20a8ce04f
isicas2025 <isicas2025@outlook.com>
2024-07-31 14:44:36 +0800

    Add files via upload

---
+++ about/about-iscas.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>IEEE ISCAS 2024 | About ISCAS</title>
+    <?php include '../include/header_1.html';?>
+</head>
+
+<body>
+    <?php include '../include/menu_1.html';?>
+
+  <main id="main">
+      
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+        
+      <div class="container">
+        <ol>
+          <li><a href="../">Home</a></li>
+          <li><a href="#">About ISCAS</a></li>
+        </ol>
+        <h2>About ISCAS</h2>
+        <p>
+            <button class="button_new2"><a href="../about/welcome-message.php" target="_blank"><i class="bi bi-arrow-up-right-square"></i> Welcome Message</a></button> 
+            &nbsp;|&nbsp;
+            <button class="button_new2"><a href="https://ieee-cas.org/celebrating-75-years-ieee-circuits-and-systems-society" target="_blank"><i class="bi bi-arrow-up-right-square"></i> CASS 75 Page</a></button> 
+            &nbsp;|&nbsp;
+            <button class="button_new2"><a href="./about-news.php" target="_blank"><i class="bi bi-arrow-up-right-square"></i> ISCAS 2024 News Release</a></button>
+            &nbsp;|&nbsp;
+            <button class="button_new2"><a href="../program/ISCAS_2024_Program_At_a_Glance.pdf?v=20240513" target="_blank"><i class="bi bi-arrow-up-right-square"></i> Program At a Glance</a></button>
+            &nbsp;|&nbsp;
+            <button class="button_new2"><a href="../program/ISCAS_2024_Final_Program.pdf?v=20240519" target="_blank"><i class="bi bi-arrow-up-right-square"></i> Final Program Book</a></button>
+        </p>
+        <br>
+      </div>
+        
+      <div class="container">
+          <div class="team4">
+
+            <div class="row">
+
+              <div class="col-lg-12 px-5">
+                <div class="member d-flex align-items-start">
+                  <div class="member-info">
+                    <p>
+                        The IEEE International Symposium on Circuits and Systems (ISCAS) is the flagship conference of the IEEE Circuits and Systems (CAS) Society and the world’s premier forum for researchers in the active fields of theory, design and implementation of circuits and systems. This is accomplished through technical conference sessions, poster sessions, live demonstration sessions, and publication of conference papers.
+                    </p>
+                    <p>
+                        ISCAS 2024 is inspired by the theme "circuits and systems for sustainable development", which is perfectly aligned with the host city's goal.
+                    </p>
+                    <p>
+                        The symposium highlights a wide range of innovatition themes including: Artificial Intelligence and Deep Learning, Smart Systems for Automotive, Brain: Innovation Neurotechnologies, Intelligent Cyber Security Systems, Food Security and Climate Change, Ultra Low Power Circuits and Systems.
+                    </p>
+                    <p>
+                        Special initiatives include: Emerging Technologies Workshops on Information Security, Climate Change, and GeronCAS; Cross-Society Special Sessions which is co-organized by CASS and other IEEE societies/councils, on recent attractive themes for cross-disciplinary collaboration; and Special Sections in IEEE Transactions (invited publication in TCAS-I, TCAS-II, TBioCAS, TVLSI). (<a href="../authors/journal-special-issue.php" target="_blank">Details on Journal Special Issue</a>)
+                    </p>
+                    <p>
+                        The CAS Society will celebrate the 75th anniversary in ISCAS 2024.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+            </div><!-- End row -->
+
+          </div><!-- End team2 -->
+      </div><!-- End container -->
+        
+    </section><!-- End Breadcrumbs -->
+      
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+      <?php include '../include/footer_1.html';?>
+      <?php include '../include/credit.html';?>
+      <?php include '../include/timestamp.php';?>
+  </footer><!-- End Footer -->
+
+    <?php include '../include/script_1.html';?>
+
+</body>
+
+</html>
