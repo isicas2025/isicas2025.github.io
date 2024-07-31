5cc44aca9eefdbf81bdb66b36d6a66e52944b928
isicas2025 <isicas2025@outlook.com>
2024-07-31 14:44:48 +0800

    Add files via upload

---
+++ authors/author-instructions.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>IEEE ISCAS 2024 | Author: Instructions</title>
+    <?php include '../include/header_1.html';?>
+</head>
+
+<body>
+    <?php include '../include/menu_1.html';?>
+
+  <main id="main">
+      
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+        
+      <div class="container">
+        <ol>
+          <li><a href="../">Home</a></li>
+            <li><a href="#">Author: Instructions</a></li>
+        </ol>
+        <h2>Author Instructions</h2>
+        <br>
+      </div>
+        
+      <div class="container">
+          <div class="team4">
+            <div class="row">
+              <div class="col-lg-12 px-5">
+                <div class="member d-flex align-items-start">
+                  <div class="member-info">
+                    <h3>Submissions</h3>
+                    <p>
+                        <li>ALL submissions must be made on-line from ePapers <a href="https://epapers2.org/iscas2024" target="_blank">submission site</a>.</li>
+                        <li>Email submissions will <u>NOT</u> be accepted.</li>
+                        <li>If you are making multiple submissions, you only need to create one web account. Use the same account (User name &amp; Password) for any additional submissions.</li>
+                        <li>If you need to edit any author information or resubmit your paper, it may be done up until the submission deadline.</li>
+                    </p>
+                    <br>
+                    <h3>Paper Specification</h3>
+                    <p>
+                        <font color = "#FF0000"><b>THIS IS A DOUBLE BLIND REVIEW PROCESS. Do NOT include author names nor affiliation information in your paper.</b></font>
+                        <br><br>
+                        <b>
+                        All authors MUST adhere to the following guidelines to conceal their identity:
+                        <li>Do NOT include any authors’ information such as name, affiliation, e-mail, etc. in the initial manuscript.</li>
+                        <li>Do NOT include any grant information or personal acknowledgments in the initial manuscript.</li>
+                        <li>DO NOT contact the technical program committee members to solicit input on your manuscript.</li>
+                        <li>All references including authors’ previous work should be referred to as 3rd-persons’ works. Work that is substantially related to the submission and has been submitted to another Conference/Journal, but has not been published yet, must be cited in an anonymized format. Do not cite patents. </li>
+                        </b>
+                        <br>
+                        <b>Use our <a href="https://epapers2.org/iscas2024/ESR/samples.php" target="_blank">templates</a> to meet formatting requirements below. Your paper must comply with the following specifications:</b>
+                        <li>Paper Length: <b><i>Maximum 5 pages</i></b>, of which 4 are for technical content including text, figures and references (if any), and with <b><i>one additional optional 5th page containing only references</i></b></li>
+                        <li>Paper Size: US Letter (8.5" x 11") or A4 (210mm X 297mm)</li>
+                        <li>Fonts: Embed ALL fonts in your PDF file. Avoid the use of Type 3 fonts.</li>
+                        <li>File Format: Adobe PDF (.pdf)</li>
+                        <li>Allowed File Size: 10 MB</li>
+                        <li>Do NOT page number your paper</li>
+                        <li>Do NOT apply security settings to your PDF file.</li>
+                        <li><b><i>Note: Deviations from the above paper specifications could impede the review of your paper and result in rejection.</i></b></li>
+                    </p>
+                    <br>
+                    <h3>For Further Assistance</h3>
+                    <p>
+                        <li>In order to help you meet the above paper specifications, please use the templates which can be found on the <a href="https://epapers2.org/iscas2024/ESR/samples.php" target="_blank">Templates page.</a></li>
+                        <li>If you are having problems creating your file, please link to the <a href="https://epapers2.org/iscas2024/ESR/author_help.php" target="_blank">author help page</a>.</li>
+                    </p>
+                    <br>
+                    <h3>Additional Requirements</h3>
+                    <p>
+                        If your paper is accepted for publication at this conference, you will also need to adhere to the following requirements:
+                        <li>IEEE policy requires you to file a copyright release for each accepted submission. Without a copyright release on file, your paper CANNOT be published. Further instructions will be included in your decision email.</li>
+                        <li><b>REMINDER:</b> One author per accepted submission must be registered at the full conference rate <b>AND</b> attend the conference to present their paper.</li>
+                        <li><font color = "#FF0000"><b><i>IEEE reserves the right to exclude any accepted paper from distribution after the conference (including, removal from IEEE Xplore) if the accepted paper is not presented at the conference.</i></b></font></li>
+                    </p>
+
+                    <br>
+
+                    <p>
+                        Source: <a href="https://epapers2.org/iscas2024/ESR/authorinfo1.php" target="_blank">ISCAS 2024 Author Information</a> on ePapers.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+            </div><!-- End row -->
+
+          </div><!-- End team2 -->
+      </div><!-- End container -->
+        
+    </section><!-- End Breadcrumbs -->
+      
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+      <?php include '../include/footer_1.html';?>
+      <?php include '../include/credit.html';?>
+      <?php include '../include/timestamp.php';?>
+  </footer><!-- End Footer -->
+
+    <?php include '../include/script_1.html';?>
+
+</body>
+
+</html>
---
+++ authors/journal-special-issue.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>IEEE ISCAS 2024 | Author: Journal Special Issue</title>
+    <?php include '../include/header_1.html';?>
+</head>
+
+<body>
+    <?php include '../include/menu_1.html';?>
+
+  <main id="main">
+      
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+        
+      <div class="container">
+        <ol>
+          <li><a href="../">Home</a></li>
+            <li><a href="#">Author: Journal Special Issue</a></li>
+        </ol>
+        <h2>Journal Special Issue</h2>
+        <br>
+      </div>
+        
+      <div class="container">
+          <div class="team4">
+            <div class="row">
+              <div class="col-lg-12 px-5">
+                <div class="member d-flex align-items-start">
+                  <div class="member-info">
+                    <p>
+                        Authors of selected accepted ISCAS 2024 papers will be invited to submit a paper for possible publication either in:
+                    </p>
+                    <p>
+                        <li>
+                            <a href="https://ieee-cas.org/publication/TCAS-I" target="_blank">IEEE Transactions on Circuits and Systems I: Regular Papers (TCAS-I)</a>
+                        </li>
+                        <li>
+                            <a href="https://ieee-cas.org/publication/TCAS-II" target="_blank">IEEE Transactions on Circuits and Systems II: Express Briefs (TCAS-II)</a>
+                        </li>
+                        <li>
+                            <a href="https://ieee-cas.org/publication/TBioCAS" target="_blank">IEEE Transactions on Biomedical Circuits and Systems (TBioCAS)</a>
+                        </li>
+                        <li>
+                            <a href="https://ieee-cas.org/publication/TVLSI" target="_blank">IEEE Transactions on Very Large Scale Integration Systems (TVLSI)</a>
+                        </li>
+                    </p>
+                    <p>
+                        Invitations will be made by a team of Guest Associate Editors on the basis of the timeliness and quality of technical results presented, the outcome of the review process and the recommendations of the Review Committee Members (RCMs) and Track Chairs (TCs) who were involved in the review process itself.
+                    </p>
+                    <p>
+                        Authors of the selected papers will receive an invitation email with detailed instructions on how to submit a manuscript derived from their ISCAS paper to the Scholar One web site for one of the participating journals. 
+                        <li>
+                            For invitations to TCAS-I, or TBioCAS, or TVLSI, the new manuscript shall be a substantially expanded version of the corresponding ISCAS paper.
+                        </li>
+                        <li>
+                            For invitations to TCAS-II, the new manuscript shall be a minor revision of the ISCAS paper (the maximum length of a TCAS-II paper is five pages only).
+                        </li>
+                    </p>
+                    <br>
+                    <h3>Important Note</h3>
+                    <p><b>
+                        Papers submitted to TCAS-II, if accepted, will represent the only record published in IEEE Xplore. In other words, the corresponding ISCAS paper, being largely overlapped, will not appear in the ISCAS 2024 version of the Proceedings uploaded to IEEE Xplore. Author registration requirements will be the same as for regular ISCAS papers, as well as the implicit obligation to present the paper during the conference.
+                        </b>
+                    </p>
+                    <p>
+                        Authors who do not wish to consider this option for publication will have, at submission time, the possibility to opt-out from having their ISCAS submission considered for possible invitation to TCAS-II.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+            </div><!-- End row -->
+
+          </div><!-- End team2 -->
+      </div><!-- End container -->
+        
+        
+    </section><!-- End Breadcrumbs -->
+      
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+      <?php include '../include/footer_1.html';?>
+      <?php include '../include/credit.html';?>
+      <?php include '../include/timestamp.php';?>
+  </footer><!-- End Footer -->
+
+    <?php include '../include/script_1.html';?>
+
+</body>
+
+</html>
